6732f4ed48a0a45760c71e4afe083be6bbad6228
Bhargav <saibhargav9090@gmail.com>
2026-07-15 22:43:40 +0530

    Allow direct execution of Flask app using python app.py on

--- app.py
+++ app.py
@@ -268,5 +268,6 @@ def run_redteam_simulation():
     return Response(generate_simulation(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
